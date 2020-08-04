57fe877462d01d3ec5cc4b6a229ba8266edecbb1
Kwothsei <45246543+Kwothsei@users.noreply.github.com>
2020-08-04 20:59:45 +0200

    Big changes

--- Controllers/PoliceController.cs
+++ Controllers/PoliceController.cs
@@ -50,7 +50,6 @@ namespace AOGPD.Controllers
         {
             if (ModelState.IsValid)
             {
-
                 var uppercaseBolo = new Bolo
                 {
                     LicensePlate = bolo.LicensePlate.ToUpper(),
